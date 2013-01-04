9cde9d3c1ce0784ea5d8d93acd31a109cc55c097
kis2012dsh@gmail.com <kis2012dsh@gmail.com@d6ab9e65-46ae-c564-aa41-84b3480532be>
2013-01-04 13:36:00 +0000

    changes

--- branches/my_utility/stringsetTest.cpp
+++ branches/my_utility/stringsetTest.cpp
@@ -1,118 +1,104 @@
-#include "utility.h"
-#include "stringset.h"
-#include <iostream>
-#include <string>
-#include <set>
+#include <my_utility\stringset.h>
+#include <my_utility\utility.h>
 #include <my_utility\timer.h>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <iterator>
-#include <cmath>
-#include <fstream>
-#include <unordered_set>
+#include <my_utility\set.h>
 
-const int N = 4000000 ;
-const double eps = 1e-5 ;
+#include <iostream>
+#include <set>
+#include <string>
+#include <random> 
 
-std::string getRandom( )
-{
-	int len( (rand() % 10) + 1);
-	std::string ret("");
-	for(int i = 0 ; i < len ; ++i ) ret.append(1 , char('a') + (rand() % 26)) ;
-	return ret ;
-}
+const int N = 2000000 ;
+const int minLen = 3;
+const int maxLen = 5;
+
+template<typename charHash>
+class RandomString {
+public:
+	RandomString( std::size_t _min , std::size_t _max  )
+		: bMin(_min) , bMax( _max ) , hasher(),rd(),gen(rd()),strLenGen(bMin,bMax)
+		,chrValGen( 1 , hasher.max_value() )  // 1 to 26
+	{ }
+public:
+	inline std::string getString() 
+	{
+		std::size_t strLen(strLenGen(gen));
+		std::string result(strLen , ' ') ;
+
+		for(std::size_t i = 0 ; i != strLen ; ++i )
+			result.at(i) = 'a' + (chrValGen(gen) - 1) ;
+
+		return std::move( result );
+	}
+private:
+	std::size_t bMin , bMax ;
+	charHash    hasher ;
+
+	std::random_device rd ;
+	std::mt19937 gen ;
+	std::uniform_int_distribution<> strLenGen ;
+	std::uniform_int_distribution<> chrValGen ;
+};
 
 int main()
 {
-	std::srand( std::time(0)) ;
-	std::vector<std::string> strvec(N);
+	// 测试的两种容器
+	DSH::stringset<DSH::TR1::alpha<char,true>> x ;
+	std::set<std::basic_string<char>>          y ;
 
-	{
-		std::ifstream testfile("F:\\stringset_data\\phone.txt");
-		for(int i = 0 ; i < N ; ++i ) {
-			std::getline( testfile , strvec[i] );
-		}
-		testfile.close();
-	}
-
+	// 测试辅助工具
 	DSH::timer mTimer ;
-	DSH::stringset<DSH::TR1::number<char>,true>  x ;
-	std::set<std::string>                        y ;
-//	std::unordered_set<std::string>            y ;
+	std::vector<std::basic_string<char>> strArray( N ) ;
 
-	std::cout << "导入手机号码个数 N = " << N << std::endl; 
-//	std::cout << "去掉重复后个数 M = " << strvec.size() << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "插入 速度测试 : " << std::endl;
-	mTimer.reset() ;
+	// 生成随机字符串
+	RandomString<DSH::TR1::alpha<char,true>> randStr(minLen , maxLen) ;
 	for(int i = 0 ; i < N ; ++i )
-	{
-		x.insert( strvec[i] );
-	}
-	std::cout << "My StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
-	mTimer.reset() ;
-	for(int i = 0 ; i < N  ;++i )
-	{
-		y.insert( strvec[i] );
-	}
-	std::cout << "STL StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
+		strArray.at(i) = std::move(randStr.getString());
 
+	////////////////////////////////////////////////////////
+	//   测试开始
+	////////////////////////////////////////////////////////  
+
+	std::cout << "N = " << N << std::endl;
+
+	// 插入测试
 	std::cout << std::endl;
-	std::cout << "查找 速度测试 : " << std::endl;
-	mTimer.reset() ;
-	for(int i = 0 ; i < N ; ++i )
-	{
-		x.find( strvec[i] );
-	}
-	std::cout << "My StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
-	mTimer.reset() ;
-	for(int i = 0 ; i < N  ;++i )
-	{
-		y.find( strvec[i] );
-	}
-	std::cout << "STL StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
+	std::cout << "Insert : " << std::endl;
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		x.insert( strArray[i] );
+	std::cout << "My String Set  : " << mTimer.elapsed() << " s" << std::endl;
 
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		y.insert( strArray[i] );
+	std::cout << "STL String Set : " << mTimer.elapsed() << " s" << std::endl;
+
+	// 查询测试
 	std::cout << std::endl;
-	std::cout << "字典序输出 : " << std::endl;
-	std::vector<std::string> outputx ;
-	std::vector<std::string> outputy ;
+	std::cout << "Search : " << std::endl;
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		x.find( strArray[i] );
+	std::cout << "My String Set  : " << mTimer.elapsed() << " s" << std::endl;
 
-	double x_time , y_time ; 
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		y.find( strArray[i] );
+	std::cout << "STL String Set : " << mTimer.elapsed() << " s" << std::endl;
 
-	mTimer.reset() ;
-	x.lexicographical_copy( std::back_inserter(outputx) );
-//	x.depth_first_copy( std::back_inserter(outputx)) ;
-	x_time = mTimer.elapsed() ;
-
-	mTimer.reset() ;
-	std::copy( y.begin() , y.end() , std::back_inserter(outputy) );
-	y_time = mTimer.elapsed();
-
-//	if( !std::equal( outputx.begin() , outputx.end() , y.begin() ) )
-//		std::cout << "非字典序" << std::endl;
-//	else {
-		x_time = (fabs(x_time) < eps) ? (0) : (x_time) ;
-		y_time = (fabs(y_time) < eps) ? (0) : (y_time) ;
-		std::cout << "My StringSet : " << x_time << " 秒" << std::endl ;
-		std::cout << "STL StringSet : " << y_time << " 秒" << std::endl;
-//	}
-
+	// 删除测试
 	std::cout << std::endl;
-	std::cout << "删除 速度测试 : " << std::endl;
-	mTimer.reset() ;
-	for(int i = 0 ; i < N ; ++i )
-	{
-		x.erase( strvec[i] );
-	}
-	std::cout << "My StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
-	mTimer.reset() ;
-	for(int i = 0 ; i < N  ;++i )
-	{
-		y.erase( strvec[i] );
-	}
-	std::cout << "STL StringSet : " << mTimer.elapsed() << " 秒" << std::endl; 
-	
+	std::cout << "Delete : " << std::endl;
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		x.erase( strArray[i] );
+	std::cout << "My String Set  : " << mTimer.elapsed() << " s" << std::endl;
+
+	mTimer.reset();
+	for(int i = 0 ; i < N ; ++i ) 
+		y.erase( strArray[i] );
+	std::cout << "STL String Set : " << mTimer.elapsed() << " s" << std::endl;
+
 	return 0;
-}
+} 
